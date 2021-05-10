77fc3c66910c2e0662474ccdce2ea3a088b66990
Artem <a.khimin@yandex.ru>
2021-05-10 16:35:21 +0300

    Добавьте файлы проекта.

---
+++ Mobiledb001/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using Mobiledb001.Models;
+using System.Data.Entity;
+
+namespace Mobiledb001
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MobileContext db;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new MobileContext();
+            db.Phones.Load(); // загружаем данные
+            phonesGrid.ItemsSource = db.Phones.Local.ToBindingList(); // устанавливаем привязку к кэшу
+
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Dispose();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            db.SaveChanges();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (phonesGrid.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < phonesGrid.SelectedItems.Count; i++)
+                {
+                    Phone phone = phonesGrid.SelectedItems[i] as Phone;
+                    if (phone != null)
+                    {
+                        db.Phones.Remove(phone);
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+
+}
---
+++ Mobiledb001/Models/MobileContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobiledb001.Models
+{
+    public class MobileContext:DbContext
+    {
+        public MobileContext():base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Phone> Phones { get; set; }
+
+
+    }
+}
